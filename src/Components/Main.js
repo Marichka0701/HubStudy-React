e57ea8e50f44a758fc43e3056f6e5e8520856755
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-03-27 14:55:56 +0300

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,103 @@
+import '../Styles/main.css';
+
+const section = () => {
+  return ( 
+    <main class="section">
+      <div class="section-1">
+        <div class="section-1-info">
+            <h1 class="main-title">Learn with us!</h1>
+          <div class="main-paragraph">
+            <p>Lörem ipsum bitårat plafaspeligt. Flipperförälder susade dest alogi och kompetensväxling kvasipp. Sora
+              reanårade stereotik. Makrosk fåtufiliga förutom krorat, henifiera nydektigt, annonsblockerare. Tradade
+              prerel inte jabor bongen ande svajpa. </p>
+          </div>
+          <div class="main-buttons">
+            <button class="sign_up button">Обрати наставника</button>
+            <button class="sign_in  button">Стати наставником</button>
+          </div>
+        </div>
+      {/* <!-- <div class="images">
+        <img src="./images/image-main-1.png" alt="" />
+        <img src="./images/image-main-2.png" alt="" />
+      </div> --> */}
+      </div>
+      <div class="section-2">
+        <h2 class="title-section-2">Our benefits:</h2>
+        <div class="benefits">
+          <div class="benefit-item">
+            <div class="benefit-image">
+              <img src="./images/benefits-item-1.png" alt="" />
+            </div>
+            <div class="benefit-paragraph">
+              <p>
+                Найкрутіші наставники:
+                обирайте тих, хто з вами на одній хвилі та вміє знайти підхід до кожного
+              </p>
+            </div>
+          </div>
+          <div class="benefit-item">
+            <div class="benefit-image">
+              <img src="./images/benefits-item-2.png" alt="" />
+            </div>
+            <div class="benefit-paragraph">
+              <p>
+                Якість:
+                ми забезпечуємо кваліфікацію наших наставників, перевіряючи їхні професійні знання та досвід
+              </p>
+            </div>
+          </div>
+          <div class="benefit-item">
+            <div class="benefit-image">
+              <img src="./images/benefits-item-3.png" alt="" />
+            </div>
+            <div class="benefit-paragraph">
+              <p>
+                Різноманітність форматів: навчайтесь індивідуально з наставником, в групі або відвідуйте вебінари
+              </p>
+            </div>
+          </div>
+          <div class="benefit-item">
+            <div class="benefit-image">
+              <img src="./images/benefits-item-4.png" alt="" />
+            </div>
+            <div class="benefit-paragraph">
+              <p>
+                Широкий вибір послуг:
+                ви знайдете наставника у будь - якій сфері!
+              </p>
+            </div>
+          </div>
+          <div class="benefit-item">
+            <div class="benefit-image">
+              <img src="./images/benefits-item-5.png" alt="" />
+            </div>
+            <div class="benefit-paragraph">
+              <p>
+                Гнучкий графік:
+                обирайте час та займайтесь тоді, коли вам буде зручно
+              </p>
+            </div>
+          </div>
+          <div class="benefit-item">
+            <div class="benefit-image">
+              <img src="./images/benefits-item-6.png" alt="" />
+            </div>
+            <div class="benefit-paragraph">
+              <p>
+                Гнучкий графік:
+                обирайте час та займайтесь тоді, коли вам буде зручно
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section-3">
+        <h2 class="title-section-3">
+          <span class="white-title-section-3">Find</span> your instructor</h2> 
+      </div>
+    
+    </main>
+   );
+}
+ 
+export default section;
