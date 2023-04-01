8d37b61ad44c26cd1ee3d6a2bc90b2b6b297d7ab
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-04-01 21:33:34 +0300

    delete review-item

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -23,51 +23,6 @@ const Reviews = () => {
           </div>
         </div>
       </div>
-      <div className='review-item'>
-        <div className='image-review'>
-          <img src= {reviewItemOne} alt="" ></img>
-        </div>
-        <div className='text-review'>
-          <div className='log-review'>
-            <h3 className='log-review-item'>
-            Andrew
-            </h3>
-          </div>
-          <div className='description-review'>
-            <p>Зручна у використанні платформа, швидко знайшов наставника. Дякую!</p>
-          </div>
-        </div>
-      </div>
-      <div className='review-item'>
-        <div className='image-review'>
-          <img src= {reviewItemOne} alt="" ></img>
-        </div>
-        <div className='text-review'>
-          <div className='log-review'>
-            <h3 className='log-review-item'>
-            Andrew
-            </h3>
-          </div>
-          <div className='description-review'>
-            <p>Зручна у використанні платформа, швидко знайшов наставника. Дякую!</p>
-          </div>
-        </div>
-      </div>
-      <div className='review-item'>
-        <div className='image-review'>
-          <img src= {reviewItemOne} alt="" ></img>
-        </div>
-        <div className='text-review'>
-          <div className='log-review'>
-            <h3 className='log-review-item'>
-            Andrew
-            </h3>
-          </div>
-          <div className='description-review'>
-            <p>Зручна у використанні платформа, швидко знайшов наставника. Дякую!</p>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
    );
