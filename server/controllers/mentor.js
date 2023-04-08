b1bc811d3e7da5af1c29ba90d3c1c379c78b3470
NoobsSos <104564069+NoobsSos@users.noreply.github.com>
2023-04-08 11:29:37 +0300

    add filter

--- server/controllers/mentor.js
+++ server/controllers/mentor.js
@@ -7,5 +7,27 @@ export const getAllMentors = async (req, res) => {
     } catch (error) {
         res.status(404).json({message: error.message});
     }
+}
 
+export const getFilterMentors = async (req, res) => {
+    try {
+        let query;
+
+        const reqQuery = {...req.query};
+
+        const removeFields = ['select', 'sort', 'page', 'limit'];
+
+        // Loop over removeField and delete them
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        let queryStr = JSON.stringify(reqQuery);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+        query = Mentor.find(JSON.parse(queryStr));
+        const mentor = await query;
+        res.status(200).json({data: mentor});
+
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 }
--- server/routes/mentor.js
+++ server/routes/mentor.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllMentors } from "../controllers/mentor.js";
+import { getAllMentors,
+         getFilterMentors} from "../controllers/mentor.js";
 
 const router = express.Router();
 
 router.get("/", getAllMentors);
+router.get("/filter", getFilterMentors)
 
 export default router
